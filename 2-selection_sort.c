6b803b028ac15f1f5869364fb48245a68ebd2b25
RodrigoNovelli <Rodrigonovelli21@hotmail.com>
2024-08-09 15:33:56 +0000

    2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,26 +1,34 @@
 #include "sort.h"
-/*+
- *
+/**
+ * selection_sort - Puts the values of an
+ * array in ascending order.
+ * @array: The array you want to put in order.
+ * @size: The size of the array.
+ * Return: Void.
  */
 void selection_sort(int *array, size_t size)
 {
 	size_t x;
 	size_t y;
-	int aux = 0;
-	
-	for (x = 0; x < size; x++)
+	size_t min = 0;
+	int p = 0;
+
+	for (x = 0; x < size - 1; x++)
 	{
-		for (y = 1; y < size - x; x++)
+		min = x;
+		for (y = x + 1; y < size; y++)
 		{
-			if (array[x] > array[y])
+			if (array[min] > array[y])
 			{
-				aux = array[x];
-				array[x] =  array[y];
-				array[y] = aux;
+				min = y;
+			}
+			if (y == size - 1 && min != x)
+			{
+				p = array[x];
+				array[x] = array[min];
+				array[min] = p;
 				print_array(array, size);
 			}
-			else
-				continue;
 		}
 	}
 }
